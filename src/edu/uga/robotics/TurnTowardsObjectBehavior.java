dbee78e5f934902112f74aaeb9543215966a52a0
kbogert <kbogert@localhost>
2010-09-19 17:07:23 -0400

    Implement TurnTowardsObjectBehavior

--- src/edu/uga/robotics/TurnTowardsObjectBehavior.java
+++ src/edu/uga/robotics/TurnTowardsObjectBehavior.java
@@ -1,22 +1,49 @@
 package edu.uga.robotics;
 
+import josx.platform.rcx.Motor;
 import josx.robotics.Behavior;
 
 public class TurnTowardsObjectBehavior implements Behavior {
 
+	private final int blackThreshold = 39;
+	
 	public void action() {
-		// TODO Auto-generated method stub
+		
+		Project2a.navigator.stop();
+		Motor.A.setPower(4);
+		Motor.B.setPower(4);
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+
+		}
+		if (BackupFromObjectBehavior.lastSensorValue < blackThreshold) {
+			
+			// turn left
+			Project2a.navigator.rotate(-45.0f);
+			
+		} else {
+			// turn right
+			Project2a.navigator.rotate(45.0f);
+			
+		}		
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+
+		}
+		
+		Project2a.curState = Project2a.RobotState.Stopped;
+
 
 	}
 
 	public void suppress() {
-		// TODO Auto-generated method stub
 
 	}
 
 	public boolean takeControl() {
-		// TODO Auto-generated method stub
-		return false;
+		return Project2a.curState == Project2a.RobotState.TurnTowardsObstacle;
 	}
 
 }
