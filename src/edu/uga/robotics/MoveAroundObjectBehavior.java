0bd2eba0b348967e63ae58dc3a611298e3150084
kbogert <kbogert@localhost>
2010-09-19 16:50:46 -0400

    Add new behaviors for an expanded avoid obstacle

---
+++ src/edu/uga/robotics/MoveAroundObjectBehavior.java
@@ -0,0 +1,22 @@
+package edu.uga.robotics;
+
+import josx.robotics.Behavior;
+
+public class MoveAroundObjectBehavior implements Behavior {
+
+	public void action() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void suppress() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean takeControl() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
