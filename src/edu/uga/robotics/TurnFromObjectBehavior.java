9f7e5ed793809ebb104834fa54aaafcc641d04e7
kbogert <kbogert@localhost>
2010-09-19 17:01:43 -0400

    Implement TurnFromObjectBehavior

--- src/edu/uga/robotics/TurnFromObjectBehavior.java
+++ src/edu/uga/robotics/TurnFromObjectBehavior.java
@@ -1,22 +1,50 @@
 package edu.uga.robotics;
 
+import josx.platform.rcx.Motor;
 import josx.robotics.Behavior;
 
 public class TurnFromObjectBehavior implements Behavior {
 
+	private final int blackThreshold = 39;
+	private boolean abort = false;
+	
 	public void action() {
-		// TODO Auto-generated method stub
 
+
+		Motor.A.setPower(4);
+		Motor.B.setPower(4);
+
+		if (BackupFromObjectBehavior.lastSensorValue < blackThreshold) {
+	
+			// turn left
+			Project2a.navigator.rotate(45.0f);
+			
+		} else {
+			// turn right
+			Project2a.navigator.rotate(-45.0f);
+			
+		}
+		
+		if (abort) return;
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+
+		}
+		if (abort) return;
+		
+		Project2a.curState = Project2a.RobotState.MoveAroundObstacle;
 	}
 
 	public void suppress() {
-		// TODO Auto-generated method stub
+		abort = true;
+		Project2a.navigator.stop();
+		Project2a.curState = Project2a.RobotState.Stopped;
 
 	}
 
 	public boolean takeControl() {
-		// TODO Auto-generated method stub
-		return false;
+		return Project2a.curState == Project2a.RobotState.TurnFromObstacle;
 	}
 
 }
