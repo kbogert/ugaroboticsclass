63c2f4c456d3402b9761a6dc125d5e6524877f20
kbogert <kbogert@localhost>
2010-09-19 17:03:23 -0400

    Implement MoveAroundObjectBehavior

--- src/edu/uga/robotics/MoveAroundObjectBehavior.java
+++ src/edu/uga/robotics/MoveAroundObjectBehavior.java
@@ -1,22 +1,31 @@
 package edu.uga.robotics;
 
+import josx.platform.rcx.Motor;
 import josx.robotics.Behavior;
 
 public class MoveAroundObjectBehavior implements Behavior {
-
+	
+	private boolean abort = false;
+	
 	public void action() {
-		// TODO Auto-generated method stub
-
+		
+		Motor.A.setPower(2);
+		Motor.B.setPower(2);
+		
+		Project2a.navigator.travel(28);
+		if (abort) return;
+		
+		Project2a.curState = Project2a.RobotState.TurnTowardsObstacle;
 	}
 
 	public void suppress() {
-		// TODO Auto-generated method stub
-
+		abort = true;
+		Project2a.navigator.stop();
+		Project2a.curState = Project2a.RobotState.Stopped;
 	}
 
 	public boolean takeControl() {
-		// TODO Auto-generated method stub
-		return false;
+		return Project2a.curState == Project2a.RobotState.MoveAroundObstacle;
 	}
 
 }
