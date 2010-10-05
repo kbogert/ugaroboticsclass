169cc3482f1375e7f49b287ce747e39e7a7b29d0
kbogert <kbogert@localhost>
2010-10-04 21:05:20 -0400

    commented MoveAroundObjectBehavior.java

--- src/edu/uga/robotics/MoveAroundObjectBehavior.java
+++ src/edu/uga/robotics/MoveAroundObjectBehavior.java
@@ -2,7 +2,15 @@ package edu.uga.robotics;
 
 import josx.platform.rcx.Motor;
 import josx.robotics.Behavior;
-
+/**
+ * 
+ * Part of the more complex "avoid obstacle behavior", this 
+ * behavior is triggered after making a decision as to which way to move around
+ * a detected object.  
+ * 
+ * @author kbogert
+ *
+ */
 public class MoveAroundObjectBehavior implements Behavior {
 	
 	private boolean abort = false;
@@ -13,7 +21,7 @@ public class MoveAroundObjectBehavior implements Behavior {
 		Motor.A.setPower(2);
 		Motor.B.setPower(2);
 		
-		Project2a.navigator.travel(28);
+		Project2a.navigator.travel(28); // value provided from testing
 		if (abort) return;
 		
 		Project2a.curState = Project2a.RobotState.TurnTowardsObstacle;
