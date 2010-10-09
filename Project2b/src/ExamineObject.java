33f6992f660a74e533cfc2d4f38e6eebaf31e2a6
kbogert <kbogert@localhost>
2010-10-09 13:52:54 -0400

    Clean Up

--- Project2b/src/ExamineObject.java
+++ Project2b/src/ExamineObject.java
@@ -2,29 +2,31 @@ import com.ridgesoft.robotics.Behavior2;
 import com.ridgesoft.robotics.BehaviorListener;
 
 
+/**
+ * Get a reading from the cmucam as to the color of the object in front of us
+ * 
+ * Record the object's color in the map
+ * 
+ * If it's the one we're looking for, set an appropriate state
+ * 
+ * @author kbogert
+ *
+ */
 public class ExamineObject implements Behavior2 {
 
-	@Override
 	public void setEnabled(boolean arg0) {
-		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public void setListener(BehaviorListener arg0) {
-		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public boolean poll() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
 	public void setActive(boolean arg0) {
-		// TODO Auto-generated method stub
 
 	}
 
