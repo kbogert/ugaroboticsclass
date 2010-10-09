5788474ca918530c06a9b1ec008e85f12fb8354a
kbogert <kbogert@localhost>
2010-10-09 14:21:50 -0400

    Implement basic map

---
+++ Project2b/src/Map.java
@@ -0,0 +1,75 @@
+
+public class Map {
+
+	private MapObj [][] myMap;
+	private int xlen;
+	private int ylen;
+	
+	public Map(int sizex, int sizey) {
+		myMap = new MapObj[sizex][sizey];
+		
+		for (int i = 0; i < sizex; i ++) {
+			for (int j = 0; j < sizey; j ++) {
+				myMap[i][j] = new MapObj();
+			}
+		}
+		
+		xlen = sizex;
+		ylen = sizey;
+	}
+	
+	public MapObj getPos(int x,int y) {
+		int xshift = x + (xlen / 2);
+		int yshift = y + (ylen / 2);
+		return myMap[xshift][yshift];
+	}
+	
+	public int getMaxX() {
+		return xlen / 2; 
+	}
+	
+	public int getMinX() {
+		return - xlen / 2;
+	}
+
+	public int getMaxY() {
+		return ylen / 2;
+	}
+	
+	public int getMinY() {
+		return - ylen / 2;
+	}
+	
+	public class MapObj {
+		private byte type;
+		private boolean hasExaminedObject;
+		
+		public static final byte UNEXPLORED = -1;
+		public static final byte EMPTY_SQUARE = 0;
+		public static final byte TABLE_EDGE = 1;
+		public static final byte OBJECT = 2;
+		public static final byte HOME = 3;
+	
+		public MapObj() {
+			type = UNEXPLORED;
+			hasExaminedObject = false;
+		}
+
+		public byte getType() {
+			return type;
+		}
+
+		public void setType(byte type) {
+			this.type = type;
+		}
+
+		public boolean isHasExaminedObject() {
+			return hasExaminedObject;
+		}
+
+		public void setHasExaminedObject(boolean hasExaminedObject) {
+			this.hasExaminedObject = hasExaminedObject;
+		}
+		
+	}
+}
