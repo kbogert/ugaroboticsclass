63416986bdd0c18598c128b75e11be4fdb0b261a
kbogert <kbogert@localhost>
2010-10-09 13:46:51 -0400

    Added look around behavior

---
+++ Project2b/src/LookAround.java
@@ -0,0 +1,29 @@
+import com.ridgesoft.robotics.Behavior2;
+import com.ridgesoft.robotics.BehaviorListener;
+
+
+/**
+ * Sweep the robot around to look for objects.
+ * 
+ * @author kbogert
+ *
+ */
+public class LookAround implements Behavior2 {
+
+	public void setEnabled(boolean arg0) {
+
+	}
+
+	public void setListener(BehaviorListener arg0) {
+
+	}
+
+	public boolean poll() {
+		return false;
+	}
+
+	public void setActive(boolean arg0) {
+
+	}
+
+}
