3903d3570b844577cfcf746e819a79dc8513a2ec
kbogert <kbogert@localhost>
2010-10-09 16:07:00 -0400

    Implement simple avoidtable behavior

--- Project2b/src/AvoidEdge.java
+++ Project2b/src/AvoidEdge.java
@@ -1,5 +1,7 @@
 import com.ridgesoft.robotics.Behavior2;
 import com.ridgesoft.robotics.BehaviorListener;
+import com.ridgesoft.robotics.Navigator;
+import com.ridgesoft.robotics.sensors.SharpGP2D12;
 
 /**
  * Halt the robot immediately if an edge of the table is detected.
@@ -11,8 +13,22 @@ import com.ridgesoft.robotics.BehaviorListener;
  */
 public class AvoidEdge implements Behavior2 {
 
+	private boolean isEnabled;
+	private boolean isActive;
+	private SharpGP2D12 forwardSensor;
+	private TableSensor leftRear;
+	private TableSensor rightRear;
+	private Navigator nav;
+	
+	AvoidEdge(Navigator nav, SharpGP2D12 forward, TableSensor leftRear, TableSensor rightRear) {
+		this.forwardSensor = forward;
+		this.leftRear = leftRear;
+		this.rightRear = rightRear;
+		this.nav = nav;
+	}
+	
 	public void setEnabled(boolean arg0) {
-
+		isEnabled = arg0;
 	}
 
 	public void setListener(BehaviorListener arg0) {
@@ -20,11 +36,24 @@ public class AvoidEdge implements Behavior2 {
 	}
 
 	public boolean poll() {
+		if (! isEnabled) {
+			return false;
+		}
+		
+		if (forwardSensor.getDistanceInches() > 12 ||
+				! leftRear.isOnTable() || ! rightRear.isOnTable()) {
+			
+			nav.stop();
+			
+			
+		}
+		
+		
 		return false;
 	}
 
 	public void setActive(boolean arg0) {
-
+		isActive = arg0;
 	}
 
 }
