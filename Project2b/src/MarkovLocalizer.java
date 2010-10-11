02eb7c73cf1ddc253f3b6825ab55b985391a4fd0
kbogert <kbogert@localhost>
2010-10-11 09:19:53 -0400

    Add markovlocalizer class

---
+++ Project2b/src/MarkovLocalizer.java
@@ -0,0 +1,37 @@
+import com.ridgesoft.robotics.Localizer;
+import com.ridgesoft.robotics.Pose;
+
+
+/**
+ * Integrates data from a number of sensors to 
+ * 
+ * @author kbogert
+ *
+ */
+public class MarkovLocalizer implements Localizer, Runnable {
+
+	public Pose getPose() {
+		return null;
+	}
+
+	public void setHeading(float arg0) {
+
+	}
+
+	public void setPose(Pose arg0) {
+
+	}
+
+	public void setPose(float arg0, float arg1, float arg2) {
+
+	}
+
+	public void setPosition(float arg0, float arg1) {
+
+	}
+
+	public void run() {
+		
+	}
+
+}
