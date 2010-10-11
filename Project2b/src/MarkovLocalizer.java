9aaa8f4bf8532159180a82fc83eb6b8f7152b00e
kbogert <kbogert@localhost>
2010-10-11 09:25:44 -0400

    Initial implementation

--- Project2b/src/MarkovLocalizer.java
+++ Project2b/src/MarkovLocalizer.java
@@ -1,15 +1,32 @@
 import com.ridgesoft.robotics.Localizer;
 import com.ridgesoft.robotics.Pose;
+import com.ridgesoft.robotics.sensors.SharpGP2D12;
 
 
 /**
- * Integrates data from a number of sensors to 
+ * Integrates data from a number of sensors to estimate the robot's position
  * 
  * @author kbogert
  *
  */
 public class MarkovLocalizer implements Localizer, Runnable {
 
+	private Localizer odometer;
+	private SharpGP2D12 objectSensor;
+	private SharpGP2D12 tableEdgeSensor;
+	private TableSensor leftTableSensor;
+	private TableSensor rightTableSensor;
+	private Map map;
+	
+	public MarkovLocalizer(Localizer odometerLocalizer, SharpGP2D12 objectSensor, SharpGP2D12 tableEdgeSensor, TableSensor leftTableSensor, TableSensor rightTableSensor, Map map) {
+		odometer = odometerLocalizer;
+		this.objectSensor = objectSensor;
+		this.tableEdgeSensor = tableEdgeSensor;
+		this.leftTableSensor = leftTableSensor;
+		this.rightTableSensor = rightTableSensor;
+		this.map = map;
+	}
+	
 	public Pose getPose() {
 		return null;
 	}
